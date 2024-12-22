e609b7480f0095aaf42d54099582bcb0429f3eba
foreverfl <forever_fl@naver.com>
2024-12-22 20:56:04 +0900

    Update materials

--- sidebars.ts
+++ sidebars.ts
@@ -1,4 +1,5 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+import { SidebarItemConfig } from "@docusaurus/plugin-content-docs/src/sidebars/types.js";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -128,6 +129,211 @@ const sidebars: SidebarsConfig = {
         },
       ],
     },
+    // 유저 가이드
+    {
+      type: "category",
+      label: "유저 가이드",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "editor/codebasics",
+          label: "편집 기본사항",
+        },
+        {
+          type: "doc",
+          id: "editor/extension-marketplace",
+          label: "확장 마켓플레이스",
+        },
+        {
+          type: "doc",
+          id: "editor/intellisense",
+          label: "IntelliSense",
+        },
+        {
+          type: "doc",
+          id: "editor/editingevolved",
+          label: "코드 탐색",
+        },
+        {
+          type: "doc",
+          id: "editor/refactoring",
+          label: "리팩토링",
+        },
+        {
+          type: "doc",
+          id: "editor/debugging",
+          label: "디버깅",
+        },
+        {
+          type: "doc",
+          id: "editor/testing",
+          label: "테스트",
+        },
+        {
+          type: "doc",
+          id: "editor/vscode-web",
+          label: "웹용 VS Code",
+        },
+        {
+          type: "doc",
+          id: "editor/tasks",
+          label: "작업",
+        },
+        {
+          type: "doc",
+          id: "editor/profiles",
+          label: "프로필",
+        },
+        {
+          type: "doc",
+          id: "editor/settings-sync",
+          label: "설정 동기화",
+        },
+        {
+          type: "doc",
+          id: "editor/userdefinedsnippets",
+          label: "스니펫",
+        },
+        {
+          type: "doc",
+          id: "editor/emmet",
+          label: "Emmet 사용",
+        },
+        {
+          type: "doc",
+          id: "editor/command-line",
+          label: "명령줄 인터페이스",
+        },
+        {
+          type: "doc",
+          id: "editor/workspaces",
+          label: "작업공간",
+        },
+        {
+          type: "doc",
+          id: "editor/workspace-trust",
+          label: "작업공간 신뢰 설정",
+        },
+        {
+          type: "doc",
+          id: "editor/multi-root-workspaces",
+          label: "다중 루트 작업공간",
+        },
+        {
+          type: "doc",
+          id: "editor/accessibility",
+          label: "접근성",
+        },
+        {
+          type: "doc",
+          id: "editor/voice",
+          label: "음성 상호작용",
+        },
+        {
+          type: "doc",
+          id: "editor/custom-layout",
+          label: "사용자 지정 레이아웃",
+        },
+        {
+          type: "doc",
+          id: "editor/port-forwarding",
+          label: "포트 포워딩",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Git",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "터미널",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "GitHub Copilot",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "프로그래밍 언어",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Node.js / JavaScript",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "TypeScript",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Python",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
     {
       type: "category",
       label: "Java",
@@ -203,6 +409,97 @@ const sidebars: SidebarsConfig = {
           type: "doc",
           id: "java/java-faq",
           label: "FAQ",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "C++",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "C#",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "DOCKER",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "데이터 과학",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "AZURE",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "원격 개발",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Dev 컨테이너",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          type: "doc",
+          id: "java/java-tutorial",
+          label: "시작하기",
         },
       ],
     },
